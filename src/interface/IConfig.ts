6d8d1c56125708fb5976fbdbdbcd0a8adb46e560
duwei <905571995@qq.com>
2018-08-27 13:32:00 +0800

    feat: add config interface

---
+++ src/interface/IConfig.ts
@@ -0,0 +1,18 @@
+import { ConnectionOptions } from 'mongoose';
+
+export interface IConfig {
+  env: 'development' | 'production' | 'test';
+  port: number;
+  ip: string;
+  mongo: {
+    uri: string;
+    options: ConnectionOptions;
+  },
+  appid: string;
+  appSecret: string;
+  jwtSecret: string;
+  jwtExpires: string;
+  pagination: {
+    limit: number;
+  }
+}
